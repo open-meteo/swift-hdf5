67f83f1e64ceb9d9943b6c92e0075c73a961840d
terraputix <terraputix@mailbox.org>
2026-03-09 12:52:05 +0100

    constrain type in writeDataset

--- Sources/SwiftHDF5/HDF5Dataset.swift
+++ Sources/SwiftHDF5/HDF5Dataset.swift
@@ -7,7 +7,7 @@ public final class HDF5Dataset: Sendable {
         self.parent = parent
     }
 
-    public func writeDataset<T: Sendable>(data: [T]) async throws {
+    public func writeDataset<T: Numeric & Sendable>(data: [T]) async throws {
         try await HDF5.h5Dwrite(dataset: id, data: data)
     }
 
